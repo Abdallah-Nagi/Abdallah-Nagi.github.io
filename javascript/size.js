d23b0ec66259ecc75c9f9ab1b116ec899f711423
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2022-05-28 15:23:32 +0200

    update

--- javascript/size.js
+++ javascript/size.js
@@ -1,8 +1,8 @@
 let container = document.querySelector(".blocks-container");
 let c, r, size;
 window.addEventListener("load", () => {
-  let height = window.innerHeight * 0.7;
-  let width = window.innerWidth * 0.6;
+  let height = window.innerHeight * 0.8;
+  let width = window.innerWidth * 0.65;
   if (height > width) {
     r = 5;
     c = 4;
@@ -16,19 +16,19 @@ window.addEventListener("load", () => {
   }
 });
 
-window.addEventListener("resize", () => {
-  console.log(window.innerHeight);
-  let height = window.innerHeight * 0.7;
-  let width = window.innerWidth * 0.6;
-  if (height > width) {
-    r = 5;
-    c = 4;
-    size = width / c;
-    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
-  } else {
-    r = 4;
-    c = 5;
-    size = height / r;
-    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
-  }
-});
+// window.addEventListener("resize", () => {
+//   console.log(window.innerHeight);
+//   let height = window.innerHeight * 0.8;
+//   let width = window.innerWidth * 0.65;
+//   if (height > width) {
+//     r = 5;
+//     c = 4;
+//     size = width / c;
+//     container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+//   } else {
+//     r = 4;
+//     c = 5;
+//     size = height / r;
+//     container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+//   }
+// });
