56fdb81cb64b2f433842d166d902f29108112b39
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2022-05-28 13:46:46 +0200

    added js folder

---
+++ javascript/size.js
@@ -0,0 +1,34 @@
+let container = document.querySelector(".blocks-container");
+let c, r, size;
+window.addEventListener("load", () => {
+  let height = window.innerHeight * 0.7;
+  let width = window.innerWidth * 0.6;
+  if (height > width) {
+    r = 5;
+    c = 4;
+    size = width / c;
+    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+  } else {
+    r = 4;
+    c = 5;
+    size = height / r;
+    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+  }
+});
+
+window.addEventListener("resize", () => {
+  console.log(window.innerHeight);
+  let height = window.innerHeight * 0.7;
+  let width = window.innerWidth * 0.8;
+  if (height > width) {
+    r = 5;
+    c = 4;
+    size = width / c;
+    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+  } else {
+    r = 4;
+    c = 5;
+    size = height / r;
+    container.style.cssText = `grid-template-columns: repeat(${c}, ${size}px);grid-template-rows: repeat(${r}, ${size}px);`;
+  }
+});
